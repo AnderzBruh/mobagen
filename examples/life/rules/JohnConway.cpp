00c2336cd1c1b25f110f328092c4659c43002f9b
Anders Muecke <lukas.muecke@mymail.champlain.edu>
2025-09-09 14:21:16 -0400

    started game of life

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -6,6 +6,15 @@ void JohnConway::Step(World& world) {
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
+
+	int num = 0;
+
+	for (int i = -1; i <= 1; i++) {
+		for (int j = -1; j <= 1; j++) {
+            num += world.Get(Point2D(point.x + i, point.y + j));
+		}
+	}
+        num -= world.Get(point);
   // todo: implement
-  return 0;
+  return num;
 }
