3096b0340dab70a3880480bd1f09447cae8433a5
AnderzBruh <mueckeman@gmail.com>
2025-10-02 01:04:49 -0400

    Did maze generation but I think it did it wrong

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -2,11 +2,55 @@
 #include "Random.h"
 #include "RecursiveBacktrackerExample.h"
 #include <climits>
+#include <algorithm>
 bool RecursiveBacktrackerExample::Step(World* w) {
 
+  if (stack.size() > 0) {
+    Point2D nowPoint = stack[stack.size()-1];
 
+    w->SetNodeColor(nowPoint,Color32(255,0,0,255));
+   // w->SetWest(nowPoint + Point2D(1,0),false);
+
+    std::vector<Point2D> visitables = getVisitables(w, nowPoint);
+
+    //std::cout << visitables.size() << std::endl;
+
+    for (auto visitable : visitables) {
+    //  w->SetNodeColor(visitable,Color32(255,255,0,255));
+
+    }
+
+    if (visitables.size() > 0) {
+
+      Point2D nextPoint = visitables[rand() % visitables.size()];
+      stack.push_back(nextPoint);
+
+      Point2D offset =  nowPoint - nextPoint;
+
+      if (offset == Point2D(1,0)){w->SetWest(nowPoint,false); std::cout << "Removed West Wall" << std::endl;}
+      if (offset == Point2D(-1,0)){w->SetEast(nowPoint,false); std::cout << "Removed East Wall" << std::endl;}
+      if (offset == Point2D(0,1)){w->SetNorth(nowPoint,false); std::cout << "Removed North Wall" << std::endl;}
+      if (offset == Point2D(0,-1)){w->SetSouth(nowPoint,false); std::cout << "Removed South Wall" << std::endl;}
+
+
+    }else {
+      stack.insert(stack.begin(),stack[stack.size()-1]);
+      stack.pop_back();
+      w->SetNodeColor(nowPoint,Color32(0,0,0,255));
+
+    }
+
+  }else {
+
+    Point2D newPoint = randomStartPoint(w);
+
+    std::cout << "Added random start point: " << newPoint.x << "," << newPoint.y << std::endl;
+
+    stack.push_back(newPoint);
+  //  return false;
+  }
   // todo: implement this
-  return false;
+  return true;
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
@@ -36,10 +80,12 @@ std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const 
   std::vector<Point2D> visitables;
 
   // todo: implement this
-  if (w->GetNorth(p)){visitables.push_back(p);}
-  if (w->GetEast(p)){visitables.push_back(p);}
-  if (w->GetSouth(p)){visitables.push_back(p);}
-  if (w->GetWest(p)){visitables.push_back(p);}
+
+
+  if (p.y < sideOver2 && std::count(stack.begin(), stack.end(), p + Point2D(0,1)) == 0){visitables.push_back(p + Point2D(0,1));}
+  if (p.x < sideOver2 && std::count(stack.begin(), stack.end(), p + Point2D(1,0)) == 0){visitables.push_back(p + Point2D(1,0));}
+  if (p.y > -sideOver2 && std::count(stack.begin(), stack.end(), p + Point2D(0,-1)) == 0){visitables.push_back(p + Point2D(0,-1));}
+  if (p.x > -sideOver2 && std::count(stack.begin(), stack.end(), p + Point2D(-1,0)) == 0){visitables.push_back(p + Point2D(-1,0));}
 
   return visitables;
 }
