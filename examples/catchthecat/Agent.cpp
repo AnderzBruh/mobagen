52e2d5eb64e7ab6e97599edcdec595b78830778f
AnderzBruh <mueckeman@gmail.com>
2025-10-10 11:16:23 -0400

    finished path finding but it is crashing :(

--- examples/catchthecat/Agent.cpp
+++ examples/catchthecat/Agent.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <queue>
 #include "World.h"
+#include "datastructures/Vector.h"
 using namespace std;
 std::vector<Point2D> Agent::generatePath(World* w) {
   unordered_map<Point2D, Point2D> cameFrom;  // to build the flowfield and build the path
@@ -17,18 +18,38 @@ std::vector<Point2D> Agent::generatePath(World* w) {
   Point2D borderExit = Point2D::INFINITE;  // if at the end of the loop we dont find a border, we have to return random points
 
   while (!frontier.empty()) {
-    // get the current from frontier
-    // remove the current from frontierset
-    // mark current as visited
-    // getVisitableNeightbors(world, current) returns a vector of neighbors that are not visited, not cat, not block, not in the queue
-    // iterate over the neighs:
-    // for every neighbor set the cameFrom
-    // enqueue the neighbors to frontier and frontierset
-    // do this up to find a visitable border and break the loop
+    Point2D current = frontier.front();// get the current from frontier
+    frontier.pop();// remove the current from frontierset
+    visited[current] = true;// mark current as visited
+    std::vector<Point2D> visitables =  getVisitableNeightbors(w, current); // returns a vector of neighbors that are not visited, not cat, not block, not in the queue
+    for (auto visitable : visitables) {// iterate over the neighs:
+       cameFrom[visitable] = current;   // for every neighbor set the cameFrom
+       frontier.push(visitable);// enqueue the neighbors to frontier and frontierset
+        frontierSet.insert(visitable);
+    }
+    if (w->catWinsOnSpace(current)){borderExit = current; break;}// do this up to find a visitable border and break the loop
   }
 
-  // if the border is not infinity, build the path from border to the cat using the camefrom map
+  std::vector<Point2D> path;
+  if (borderExit != Point2D::INFINITE) {    // if the border is not infinity, build the path from border to the cat using the camefrom map
+    cout << "found an exit" << endl;
+    Point2D current = borderExit;
+    while (cameFrom[current] != catPos) {
+      path.push_back(current);
+      current = cameFrom[current];
+    }
+
+  }
   // if there isnt a reachable border, just return empty vector
   // if your vector is filled from the border to the cat, the first element is the catcher move, and the last element is the cat move
-  return vector<Point2D>();
+  return path;
+}
+
+std::vector<Point2D> Agent::getVisitableNeightbors(World* w, Point2D current)
+{
+  std::vector<Point2D> visitables;
+  for (auto neighbor : w->neighbors(current)) {
+    if (w->catCanMoveToPosition(current)){visitables.push_back(neighbor);}
+  }
+  return visitables;
 }
